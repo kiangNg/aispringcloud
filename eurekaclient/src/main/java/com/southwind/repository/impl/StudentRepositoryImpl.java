00110b04efbf5457894a363f82ad2d03b9251eb5
kiangNg <48704615+kiangNg@users.noreply.github.com>
2019-11-26 11:45:32 +0800

    Initial commit

---
+++ eurekaclient/src/main/java/com/southwind/repository/impl/StudentRepositoryImpl.java
@@ -0,0 +1,40 @@
+package com.southwind.repository.impl;
+
+import com.southwind.entity.Student;
+import com.southwind.repository.StudentRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@Repository
+public class StudentRepositoryImpl implements StudentRepository {
+
+    private static Map<Long,Student> studentMap;
+    static {
+        studentMap = new HashMap<>();
+        studentMap.put(1l, new Student(1l , "zhangsan", 22));
+        studentMap.put(2l, new Student(2l , "lisi", 23));
+        studentMap.put(3l, new Student(3l , "zhaoliu", 23));
+    }
+    @Override
+    public Collection<Student> findAll() {
+        return studentMap.values();
+    }
+
+    @Override
+    public Student findById(long id) {
+        return studentMap.get(id);
+    }
+
+    @Override
+    public void saveOrUpdate(Student student) {
+        studentMap.put(student.getId(),student);
+    }
+
+    @Override
+    public void deleteById(long id) {
+        studentMap.remove(id);
+    }
+}
